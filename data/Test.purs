74a4fe22fd66cdfbf3a93c5496e73b76650b0936
kRITZCREEK <christoph.hegemann1337@gmail.com>
2017-02-12 21:55:10 +0100

    add record pun examples

--- data/Test.purs
+++ data/Test.purs
@@ -3,18 +3,19 @@ module MorganaTest where
 hello = 1 + 2
 
 whatever :: Int
-
 whatever x = do
   hello + show x
   hello again x
 
-  What (Just return) <- x test
+  What (Just {hello, whatever: x}) <- x test
 
   -- x
-  -- (delete-all-overlays)
-
   x asd
+  x {hello}
 
   pure return
 
 asd = asdvfc
+
+
+  -- (delete-all-overlays)
